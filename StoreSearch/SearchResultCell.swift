c37fc7f4a53112b914f116fe7b8c971284b8a57c
DevXris <chuwei0915@gmail.com>
2016-11-21 16:17:44 +0800

    Download thumbnail image with URLSessionDownloadTask by UIImageView extension func loadImage(url) -> URLSessionDownloadTask

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SearchResultCell: UITableViewCell {
     
+    // MARK: Initialization
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -18,7 +20,49 @@ class SearchResultCell: UITableViewCell {
         selectedBackgroundView = selectedView
     }
 
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+    
+    // MARK: Outlets and Properties
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
+    
+    var downloadTask: URLSessionDownloadTask?
+    
+    // MARK: Functions
+    
+    func configure(for searchResult: SearchResult) {
+        nameLabel.text = searchResult.name
+        if searchResult.artistName.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
+        }
+        artworkImageView.image = #imageLiteral(resourceName: "Placeholder")
+        if let smallURL = URL(string: searchResult.artworkSmallURL) {
+            downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
+    }
+    
+    func kindForDisplay(_ kind: String) -> String {
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: return kind
+        }
+    }
 }
