3a030463c54144307c16e6bb629d36c5ee4e8fd9
DevXris <chuwei0915@gmail.com>
2016-11-21 11:28:51 +0800

    Rename ViewController to SearchViewController and MVC folders

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Chris Huang on 21/11/2016.
+//  Copyright © 2016 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    // MARK: Outlets and Properties
+    
+    
+    
+    // MARK: Initializations
+    
+    
+    
+    // MARK: ViewController Life Cycles
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    // MARK: Target Actions
+    
+    
+    
+    // MARK: Functions
+    
+    
+    
+    // MARK: Navigation
+    
+    
+    
+}
+
